2e8b8c0ef4182b968a03b6081882c824e66ca0f1
Tommaso Amadori <tommaso.amadori.dev@gmail.com>
2021-09-30 16:27:02 +0200

    added signature of external kernel

--- src/bin/kernel.cu
+++ src/bin/kernel.cu
@@ -1,4 +1,4 @@
-﻿#include "imageHandler.cuh"
+﻿#include "image_handler.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
 	pel** imgSrc;				
 
 	//imgSrc = ReadBMP(argv[1]);
-	imgSrc = ReadBMP("../assets/images/castle_bmp.bmp");
+	imgSrc = ReadBMP("src/assets/images/castle_bmp.bmp");
 
 	if (imgSrc == NULL) {
 		printf("Cannot allocate memory for the input image...\n");
