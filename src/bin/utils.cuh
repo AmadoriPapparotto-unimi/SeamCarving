a4f1c44000f906f90e94920a5680a5bc6d68265f
Tommaso Amadori <tommaso.amadori.dev@gmail.com>
2021-10-25 12:22:12 +0200

    added utils.cuh

---
+++ src/bin/utils.cuh
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
+#include "image_handler.h"
+#include "seam_carving.h"
+
+void dummyMin(seam_t* energiesArray, seam_t& output, imgProp_t* imgProp);
+
+__global__
+void min_(const seam_t* energiesArray, seam_t* outputArray, imgProp_t* imgProp, int nThreads);
+
+void report_gpu_mem();
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
+{
+    if (code != cudaSuccess)
+    {
+        printf("GPUassert: %s %d\n", cudaGetErrorString(code), line);
+        if (abort) exit(code);
+    }
+}
